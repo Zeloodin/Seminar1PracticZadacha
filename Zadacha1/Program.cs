0e6cf1bfcd7be01b428f20b62800c4f7bcb8ce6d
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-06 20:05:28 +0500

    update Zadacha 1 (Fewer lines)

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -2,40 +2,26 @@
 // int n1 = new Random().Next(0, 1) == 0 ... 0
 // int n2 = new Random().Next(10,10 or 11) == 10 ... 10
 int a = new Random().Next(0, 1000); 
-Console.WriteLine(a);
 int b = new Random().Next(0, 1000); 
-Console.WriteLine(b);
-
+Console.Write("a=");
+Console.Write(a);
 if (a > b)
 {
     int max = a;
     int min = b;
-    Console.Write("a=");
-    Console.Write(a);
     Console.Write(" больше ");
-    Console.Write("b=");
-    Console.WriteLine(b);
+}
+else if (a == b)
+{
+    int max = b;
+    int min = a;
+    Console.Write(" равен ");
 }
 else
 {
-    if (a == b)
-    {
-        int max = b;
-        int min = a;
-        Console.Write("a=");
-        Console.Write(a);
-        Console.Write(" равен ");
-        Console.Write("b=");
-        Console.WriteLine(b);
-    }
-    else
-    {
-        int max = b;
-        int min = a;
-        Console.Write("a=");
-        Console.Write(a);
-        Console.Write(" меньше ");
-        Console.Write("b=");
-        Console.WriteLine(b);
-    }
+    int max = a;
+    int min = b;
+    Console.Write(" меньше ");
 };
+Console.Write("b=");
+Console.WriteLine(b);
