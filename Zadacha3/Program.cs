daf1eaed94c4cb9933c19d7b701151a191dca9ba
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-06 21:28:15 +0500

    add Zadacha 3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+if (number2 % 2 == 0)
+{
+    Console.WriteLine($"число {number2} чётное");  
+}
+else
+{
+    Console.WriteLine($"число {number2} нечётное");  
+};
